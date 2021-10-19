3a8706d13d8af1f24b72e2841227e6789c28ca3f
Asemko-Asem06 <92802997+Asemko-Asem06@users.noreply.github.com>
2021-10-19 19:14:35 +0500

    Add files via upload

---
+++ newproject.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    long long n;
+    cin>>n;
+    cout<<"The next number for the number "<<abs(n)<<" is "<<abs(n)+1<<"."<<
+endl;
+cout<<"The previous number for the number "<<abs(n)<<" is "<<abs(n)-1<<".";
+    return 0;
+ 
+}
